f2e0895d29e340eb25bbd93570e3980783f6a665
Cyprien <cres.cyprien@gmail.com>
2023-12-13 17:14:11 +0100

    IW-93 Créez le carrousel de la page de logement

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,18 +1,12 @@
-import PropTypes from "prop-types"
 import { Link } from "react-router-dom"
 
-function Card({ title, cover }) {
+function Card({ id, title, cover }) {
   return (
-    <Link to="/lodging" className="card">
+    <Link to={`/lodging/${id}`} className="card">
       <img src={cover} alt="title" className="img-card" />
       <div className="title-card">{title}</div>
     </Link>
   )
-}
-
-Card.propTypes = {
-  title: PropTypes.string.isRequired,
-  cover: PropTypes.string.isRequired,
 }
 
 export default Card
